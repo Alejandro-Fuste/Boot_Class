394a71d348ad820f5234e08cec58be4adcd0963d
Alejandro F <afuste7@gmail.com>
2019-12-16 10:01:48 -0500

    Adding classwork folder

---
+++ Boot_Class2/PREWORK_AF/Module-6/javascript.js
@@ -0,0 +1,19 @@
+let growBox = document.getElementById('button1').addEventListener('click', function() {
+	/*document.getElementById('box').style.height = '250px';*/
+	document.getElementById('box').style.width = '250px';
+});
+
+let changeBoxColor = document.getElementById('button2').addEventListener('click', function() {
+	document.getElementById('box').style.backgroundColor = 'blue';
+});
+
+let fadeBox = document.getElementById('button3').addEventListener('click', function() {
+	document.getElementById('box').style.opacity = '.3';
+});
+
+let resetBox = document.getElementById('button4').addEventListener('click', function() {
+	/*document.getElementById('box').style.height = '150px';*/
+	document.getElementById('box').style.width = '150px';
+	document.getElementById('box').style.backgroundColor = 'orange';
+	document.getElementById('box').style.opacity = '1';
+});
