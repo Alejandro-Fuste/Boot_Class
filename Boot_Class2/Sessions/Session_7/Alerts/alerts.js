b08697ae81d3e0302b80c888c9d43cf9b2cef8d0
Alejandro F <afuste7@gmail.com>
2020-01-06 18:18:39 -0500

    Adding changes from classwork

---
+++ Boot_Class2/Sessions/Session_7/Alerts/alerts.js
@@ -0,0 +1,9 @@
+alert('We definitely Rock!');
+
+confirm('The question is, do "you" rock?');
+
+let howMuchRock = prompt('How much do you rock?');
+
+console.log(howMuchRock);
+
+alert(howMuchRock);
