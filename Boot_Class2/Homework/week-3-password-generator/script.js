b51927ab9e9e5bc1c735b811bf830dbb27188ebf
Alejandro F <afuste7@gmail.com>
2020-01-22 09:22:35 -0500

    adding new folders with new files for classwork and pictures for homework 4

--- Boot_Class2/Homework/week-3-password-generator/script.js
+++ Boot_Class2/Homework/week-3-password-generator/script.js
@@ -4,7 +4,7 @@ var generateBtn = document.querySelector('#generate');
 // Write password to the #password input
 function writePassword() {
 	// var password = generatePassword();
-	//password
+	password;
 	var passwordText = document.querySelector('#password');
 
 	passwordText.value = password;
---
+++ Boot_Class2/Sessions/Session_12/25-Stu_Render-Todos/Unsolved/script.js
@@ -0,0 +1,12 @@
+var todoInput = document.querySelector('#todo-text');
+var todoForm = document.querySelector('#todo-form');
+var todoList = document.querySelector('#todo-list');
+var todoCountSpan = document.querySelector('#todo-count');
+
+var todos = [ 'Learn HTML', 'Learn CSS', 'Learn JavaScript' ];
+
+var textContent = '';
+var countValue = todoCountSpan.innerHTML;
+
+console.log(countValue);
+function renderList() {}
