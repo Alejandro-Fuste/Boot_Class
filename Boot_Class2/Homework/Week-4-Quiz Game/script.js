36a889e0d05043bebca24a11bdeb9fe4bfca73fa
Alejandro F <afuste7@gmail.com>
2020-01-26 22:56:29 -0500

    I added code block to startScreen and emptyDiv functions. I also added variables to hold text for instructions and start button.

--- Boot_Class2/Homework/Week-4-Quiz Game/script.js
+++ Boot_Class2/Homework/Week-4-Quiz Game/script.js
@@ -4,17 +4,27 @@ $(document).ready(function() {
 	const timerEl = $('#timer');
 	const cardBodyEl = $('.card-body');
 	const cardTitleEl = $('.card-title');
+	const cardInstuctionsEl = $('#instructions');
 	const cardTextEl = $('.card-text');
 	const startBtn = $('.btn');
+	const cardInstructions =
+		'The quiz will begin once the start button is clicked. You will have 75 seconds to complete the quiz. Questions that are answered incorrectly will result in the time being reduced. When questions are answered correctly, the time it took to answer question will be recorded. Your total time will be displayed at the end of the quiz.';
+	const startBtnText = 'Start Quiz';
 
 	// Function declarations
 
-	function startScreen() {}
+	function startScreen() {
+		cardTitleEl.text('Code Quiz Challenge');
+		$('p').before(cardInstuctionsEl.detach());
+		cardTextEl.text(cardInstructions);
+		$('.card-body').append(startBtn.detach());
+	}
 
 	function emptyDiv() {
-		$('.card-title').empty();
-		$('.card-text').empty();
-		$('.btn').detach();
+		cardTitleEl.empty();
+		cardInstuctionsEl.detach();
+		cardTextEl.empty();
+		startBtn.detach();
 	}
 
 	// Event listener for Start Button
