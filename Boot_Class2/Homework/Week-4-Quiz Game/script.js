66525401f94c825178b958ba2aae0d6501e2fb7a
Alejandro F <afuste7@gmail.com>
2020-01-27 09:44:47 -0500

    added timer function to homework

--- Boot_Class2/Homework/Week-4-Quiz Game/script.js
+++ Boot_Class2/Homework/Week-4-Quiz Game/script.js
@@ -10,6 +10,7 @@ $(document).ready(function() {
 	const cardInstructions =
 		'The quiz will begin once the start button is clicked. You will have 75 seconds to complete the quiz. Questions that are answered incorrectly will result in the time being reduced. When questions are answered correctly, the time it took to answer question will be recorded. Your total time will be displayed at the end of the quiz.';
 	const startBtnText = 'Start Quiz';
+	let secondsLeft = 76;
 
 	// Function declarations
 
@@ -25,6 +26,19 @@ $(document).ready(function() {
 		cardInstuctionsEl.detach();
 		cardTextEl.empty();
 		startBtn.detach();
+	}
+
+	function setTime() {
+		timerEl.empty();
+		var timerInterval = setInterval(function() {
+			secondsLeft--;
+			timerEl.text('Timer:  ' + secondsLeft);
+
+			if (secondsLeft === 0) {
+				clearInterval(timerInterval);
+				alert('Time has expired');
+			}
+		}, 1000);
 	}
 
 	// Event listener for Start Button
