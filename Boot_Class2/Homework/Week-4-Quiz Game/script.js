095ce8c9c3947989032a003302b52cd133857212
Alejandro F <afuste7@gmail.com>
2020-01-26 19:39:59 -0500

    added selectors for card title, text, and button, and function for emptying the card body.
    git status
    clear

--- Boot_Class2/Homework/Week-4-Quiz Game/script.js
+++ Boot_Class2/Homework/Week-4-Quiz Game/script.js
@@ -1,17 +1,32 @@
 $(document).ready(function() {
+	// Variables holding selectors
 	const highscoreEl = $('#highscore');
 	const timerEl = $('#timer');
+	const cardBodyEl = $('.card-body');
+	const cardTitleEl = $('.card-title');
+	const cardTextEl = $('.card-text');
 	const startBtn = $('.btn');
 
-	console.log(highscoreEl);
-	console.log(timerEl);
-	console.log(startBtn);
+	// Function declarations
 
+	function startScreen() {}
+
+	function emptyDiv() {
+		$('.card-title').empty();
+		$('.card-text').empty();
+		$('.btn').detach();
+	}
+
+	// Event listener for Start Button
 	startBtn.on('click', function() {
+		event.preventDefault();
 		console.log('click');
 	});
 
+	// Event listener for viewing highscores
+
 	highscoreEl.on('click', function() {
+		event.preventDefault();
 		console.log('click');
 	});
 });
