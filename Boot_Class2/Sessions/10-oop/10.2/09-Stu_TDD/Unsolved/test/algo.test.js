ce0783c18f93b8a6cdd0cf8a66cdf6df58dcae72
Alejandro F <afuste7@gmail.com>
2020-03-03 14:49:06 -0500

    add new files from forked repo

--- Boot_Class2/Sessions/10-oop/10.2/09-Stu_TDD/Unsolved/test/algo.test.js
+++ Boot_Class2/Sessions/10-oop/10.2/09-Stu_TDD/Unsolved/test/algo.test.js
@@ -1,15 +1,17 @@
-const Algo = require("../algo");
+const Algo = require('../algo');
 
-describe("Algo", () => {
-  describe("reverse", () => {
+describe('Algo', () => {
+	describe('reverse', () => {
+		it('should take in a string and reverse the order of the characters in the string'),
+			() => {
+				const str = 'hello';
+				const resStr = 'olleh';
 
-  });
+				expect(str).toEqual(resStr);
+			};
+	});
 
-  describe("isPalindrome", () => {
-   
-  });
+	describe('isPalindrome', () => {});
 
-  describe("capitalize", () => {
-    
-  });
+	describe('capitalize', () => {});
 });
